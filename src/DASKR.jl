fb8473e01c6d778d7822f22141c05c1293be5ddd
ScottPJones <scottjones@alum.mit.edu>
2017-05-10 22:11:27 -0400

    Select correct dynamic library suffix

--- src/DASKR.jl
+++ src/DASKR.jl
@@ -5,13 +5,10 @@ module DASKR
 include("core.jl")
 include("common.jl")
 
-if is_windows()
-    const dllname = joinpath(dirname(dirname(@__FILE__)), "deps", "daskr$(Sys.WORD_SIZE).dll")
-else
-    const dllname = joinpath(dirname(dirname(@__FILE__)), "deps", "daskr.so")
-end
-
-function __init__() 
+const dllname = joinpath(dirname(dirname(@__FILE__)),
+                         "deps",
+                         is_windows() ? "daskr$(Sys.WORD_SIZE)" : "daskr")
+function __init__()
     global lib = Libdl.dlopen(dllname)
 end
 
