2943da25f2584cd777e5371dc045d02c22dedf41
Christopher Rackauckas <accounts@chrisrackauckas.com>
2017-10-04 12:01:29 -0700

    Fix saveat

--- src/common.jl
+++ src/common.jl
@@ -57,8 +57,11 @@ function solve{uType,duType,tType,isinplace,LinearSolver}(
     T = tspan[end]
 
     if typeof(saveat) <: Number
-        saveat_vec = convert(Vector{tType},saveat+tspan[1]:saveat:(tspan[end]-saveat))
-        # Exclude the endpoint because of floating point issues
+        if (tspan[1]:saveat:tspan[end])[end] == tspan[end]
+          saveat_vec = convert(Vector{tType},collect(tType,tspan[1]+saveat:saveat:tspan[end]))
+        else
+          saveat_vec = convert(Vector{tType},collect(tType,tspan[1]+saveat:saveat:(tspan[end]-saveat)))
+        end
     else
         saveat_vec =  convert(Vector{tType},collect(saveat))
     end
