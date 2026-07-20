5369c6823881325b62be81dc6f4ba27d9f3142bd
ChrisRackauckas-Claude <accounts@chrisrackauckas.com>
2026-07-20 05:49:39 -0400

    Use default public API docs QA in DASKR
    Co-Authored-By: Chris Rackauckas <accounts@chrisrackauckas.com>

--- test/qa/qa.jl
+++ test/qa/qa.jl
@@ -1,10 +1,7 @@
 using SciMLTesting, DASKR, Test
 using JET
 
-diffeqbase_reexports = Tuple(names(DASKR.DiffEqBase))
-
 run_qa(
     DASKR;
-    api_docs_kwargs = (; rendered = true, rendered_ignore = diffeqbase_reexports),
     explicit_imports = true,
 )
